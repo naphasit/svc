7c80b1a8c79841f5d988e18b952addaf9f94a5a5
naphasit <naphasitng@gmail.com>
2025-09-10 23:19:44 +0700

    feat(cli): setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{arg, Arg, ArgAction, ArgGroup, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("My CLI")
+        .version("1.0")
+        .author("Naphasit")
+        .about("Simple version control")
+        .subcommand(
+            Command::new("commit")
+                .about("Adds a new item")
+                .arg(arg!(-n --new <NAME> "Create a new commit"))
+                .arg(arg!(-g --goto <NAME> "Go to existed commit"))
+                .group(ArgGroup::new("commit").args(["new", "goto"]).required(true)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("commit", sub_matches)) => {
+            let new = sub_matches.get_one::<String>("new");
+            let goto = sub_matches.get_one::<String>("goto");
+            if new != None {
+                println!("New!")
+            }
+            else {
+                println!("Go to!")
+            }
+        }
+        _ => {}
+    }
 }
